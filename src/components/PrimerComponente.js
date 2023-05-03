8853756f650bc41ec97066a0e36a2bbb3ca62886
pozito31 <jessicapozo198715@gmail.com>
2023-05-03 12:21:06 +0200

    subiendo a github

---
+++ src/components/PrimerComponente.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export const PrimerComponente = () => {
+    return (
+        <div>
+            <h1>Primer componente</h1>
+            <p>Este es un texto en mi componente</p>
+        </div>
+    );
+}
